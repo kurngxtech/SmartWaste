8ea850ee65fe101d02ab31bfcd5ff1e64a5a26e0
kurngxtech <kurngxtech@gmail.com>
2026-04-22 00:18:13 +0800

    files: adding donation-hub-page component

---
+++ SavePlateApp/src/app/user-page/donation-hub-page/donation-hub-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donation-hub-page',
+  imports: [],
+  templateUrl: './donation-hub-page.html',
+  styleUrl: './donation-hub-page.css',
+})
+export class DonationHubPage {}
