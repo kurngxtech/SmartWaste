18e9098bce35e4df2b9bc60ff40ff9c425b6b8f4
kurngx <kurngxtech@gmail.com>
2026-05-03 23:05:10 +0800

    files: adding new components - app settings & user profile

---
+++ SavePlateApp/src/app/user-page/app-settings-page/app-settings-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-settings-page',
+  imports: [],
+  templateUrl: './app-settings-page.html',
+  styleUrl: './app-settings-page.css',
+})
+export class AppSettingsPage {}
